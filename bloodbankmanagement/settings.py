21609121ec746dd97af00c728b35d562acda8feb
sumit <wapka1503@gmail.com>
2021-02-13 20:44:15 +0530

    sqlite

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'bloodbankmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blood',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
